a08806481b95b8955f97f165b7d473ed5201ff4c
Iqbal Taufik Irfanda <iqbalirfanda13@gmail.com>
2022-01-03 11:05:41 +0700

    middleware

--- main.go
+++ main.go
@@ -6,13 +6,34 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type authheader struct {
+	AuthorizationHeader string `header:"Authorization"`
+}
+
+type Credential struct {
+	Username string `json:"username"`
+	Password string `json:"paassword"`
+}
+
 func main() {
 	r := gin.Default()
 	fmt.Println(r)
 
-	type authheader struct {
-		AuthorizationHeader string `header:"Authorization"`
-	}
+	r.POST("/login", func(c *gin.Context) {
+		var user Credential
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(400, gin.H{
+				"message": "can't bind struct",
+			})
+			return
+		}
+
+		if user.Username == "enigma" && user.Password == "123" {
+			c.JSON(200, gin.H{
+				"token": "123",
+			})
+		}
+	})
 
 	r.GET("/customer", func(c *gin.Context) {
 		h := authheader{}
